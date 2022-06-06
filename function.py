31ea3d40ade2b1b81f42516e0f363518a49b0c4c
ahmed as <medo0o.2004@gmail.com>
2022-06-07 02:07:06 +0300

    commit 2

---
+++ function.py
@@ -0,0 +1,12 @@
+#def liner():
+ #   print("......................")
+#print("ahmed")
+#liner()
+#print(22)
+#liner()
+
+def test(num1,num2):
+    print(int(num1)+ int(num2))
+n1= input("please enter number1 :")
+n2= input("please enter number2 :")
+test(n1,n2)
