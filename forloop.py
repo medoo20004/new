3c591b4fdaf848c3c67e50276767943464274f35
ahmed as <medo0o.2004@gmail.com>
2022-06-06 16:12:02 +0300

    commit

---
+++ forloop.py
@@ -0,0 +1,14 @@
+subject = ['arabic', 'english' , 'history']
+#print(subject)
+#print(subject[1])
+#subject.append ('new')
+#print(subject)
+#print(subject[1:3])
+#print(subject[-1])
+#subject.pop()
+#print(subject)
+#subject.pop(0)
+#print(subject)
+
+for x in subject :
+    print(x)
