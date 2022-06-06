d6563cedbd004065962c9ec27670f7021b97d3c3
ahmed as <medo0o.2004@gmail.com>
2022-06-06 15:54:21 +0300

    first commit

--- string.py
+++ string.py
@@ -1,5 +1,10 @@
-fname = 'ahmed'
-lname = 'ALASIRI'
-print(fname.upper () + ' ' + lname.lower ())
-b = 'A' * 500
-print(b)
+num_of_tries=0
+while num_of_tries !=3:
+    username= input("please enter your username: ")
+    password= input ('please enter your password: ')
+    if username == 'ahmed' and password == '123':
+        print('welcome to your page') 
+    else:
+        print('error')
+        num_of_tries +=1
+        print('you have' + str (3 - num_of_tries) + "left")
