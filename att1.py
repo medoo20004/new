34342563632eea401c0f357312bd16ce7259b145
ahmed as <medo0o.2004@gmail.com>
2022-06-07 04:25:13 +0300

    commit

---
+++ att1.py
@@ -0,0 +1,13 @@
+
+import requests
+
+dic= open('/Users/ahmed/Desktop/dic.txt', 'r')
+url = input ('please enter url: ')
+
+for x in dic:
+    r= requests.get(url + '/' + x.rstrip('/n'))
+    if r.status_code ==200:
+        print(' the page ' + url + '/' + x + 'is there')
+    else:
+        print('the page ' + url + '/' + x + 'is not there ')
+
